56173ad2b5c868a64d37e2524bc1c54f8764bc44
Nattapat Sittichai <frank_franky085@hotmail.com>
2022-02-03 09:21:23 +0700

    open server

--- lib/NetworkHandler.dart
+++ lib/NetworkHandler.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 import 'package:logger/logger.dart';
 
 class NetworkHandler {
-  String baseurl = "http://a44b-182-53-234-17.ngrok.io";
+  String baseurl = "http://3bb8-182-53-234-17.ngrok.io";
   var log = Logger();
 
   FlutterSecureStorage storage = FlutterSecureStorage();
