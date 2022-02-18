ac05232ba92a15b7ca238d771eed87b48bc9df5f
Pisit Jaiton <pisit.jaiton@mail.kmutt.ac.th>
2022-02-18 18:07:47 +0700

    fix deaun bug

--- lib/postProvider.dart
+++ lib/postProvider.dart
@@ -7,7 +7,7 @@ class PostProvider extends ChangeNotifier{
   List<dynamic>? postData;
 
   Future fetchData() async{
-    final Uri resAPIURL = Uri.parse("http://192.168.2.37:8080/home/getPost");
+    final Uri resAPIURL = Uri.parse("https://99d6-182-53-235-147.ngrok.io/home/getPost");
     http.Response response = await httpClient.get(resAPIURL);
     final Map parsedData = await json.decode(response.body.toString());
     postData = parsedData["data"];
