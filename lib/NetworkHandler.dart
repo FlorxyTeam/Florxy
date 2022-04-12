8d62093ad5f54d3c936fed03393caea93325531f
Nattapat Sittichai <frank_franky085@hotmail.com>
2022-04-12 19:04:32 +0700

    network

--- lib/NetworkHandler.dart
+++ lib/NetworkHandler.dart
@@ -9,12 +9,10 @@ class NetworkHandler {
 
   // String baseurl = "https://asia-southeast1-florxy.cloudfunctions.net/app";
   // String baseurl = "http://192.168.101.79:8080"; //nicha
-  String baseurl = "http://192.168.2.36:8080"; //deuan
+  // String baseurl = "http://192.168.2.36:8080"; //deuan
   // String baseurl = "http://192.168.2.38:8080"; //Leo
-  //String baseurl = "http://192.168.1.5:8080"; //Frank
-  //String baseurl = "http://192.168.1.130:8080"; //Frank
-
-  // String baseurl = "http://192.168.1.5:8080"; //Frank
+  String baseurl = "http://192.168.1.5:8080"; //Frank
+  //String baseurl = "http://192.168.1.130:8080"; //Aek
 
   var log = Logger();
 
